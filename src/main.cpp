1a7770b9cbd77eb6d42feb156f0819760a6fecb9
yurukute <yurukute@gmail.com>
2023-12-02 19:06:25 +0700

    Fix bug
    - Lock mutex when reading voltage.
    - Refactoring code.
    
    Branch: matrix752

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <functional>
 #include <iomanip>
 #include <iostream>
+#include <mutex>
 #include <string>
 #include <thread>
 #include <vector>
@@ -35,10 +36,18 @@ const char *tmp_topic = BOARD "/vernier/tmp-bta";
 const char *odo_topic = BOARD "/vernier/odo-bta";
 const char *fph_topic = BOARD "/vernier/fph-bta";
 
-std::vector<float> voltage_avg(read_num); // Store sum of voltage;
+std::vector<float> voltage_avg(read_num);
+std::mutex voltage_avg_mutex;
 float tmp = NAN, odo = NAN, fph = NAN;
 AMVIF08 ADC;
 mosqpp::mosquittopp matrix752;
+
+float getVout(int ch) {
+    voltage_avg_mutex.lock();
+    float vout = voltage_avg[ch];
+    voltage_avg_mutex.unlock();
+    return vout;
+}
 
 void readTemp() {
     SSTempSensor TMP;
@@ -46,14 +55,7 @@ void readTemp() {
     std::this_thread::sleep_for(seconds(response_time));
 
     while (true) {
-        float vout = voltage_avg[TMP_CH];
-        // Schematic:
-        //   Rt: Thermistor
-        //   R1: Divider resistor (Vernier's BTA protoboard)
-        //
-        // [GND] --- [Rt] ----- | ----- [R1] -----[VCC (5v)]
-        //                      |
-        //               [Analog input]
+        float vout = getVout(TMP_CH);
         if (vout > 0.0) {
             tmp = TMP.readSensor(vout);
         }
@@ -68,8 +70,7 @@ void readODO() {
     std::this_thread::sleep_for(seconds(response_time));
 
     while (true) {
-        // Calculate average
-        float vout = voltage_avg[ODO_CH];
+        float vout = getVout(ODO_CH);
         if (vout > 0.0) {
             odo = ODO.readSensor(vout);
         }
@@ -84,7 +85,7 @@ void readFPH() {
     std::this_thread::sleep_for(seconds(response_time));
 
     while (true) {
-        float vout = voltage_avg[FPH_CH];
+        float vout = getVout(FPH_CH);
         if (vout > 0.0) {
             fph = FPH.readSensor(vout);
         }
@@ -94,21 +95,23 @@ void readFPH() {
 }
 
 void readVoltage() {
-    std::vector<float> voltage_read;
-    voltage_read = ADC.readVoltage(1, read_num);
-    if (voltage_read.empty()) {
-        return;
-    }
-    std::transform(voltage_avg.begin(), voltage_avg.end(),
-                   voltage_read.begin(),
-                   voltage_avg.begin(), std::plus<float>());
+    voltage_avg_mutex.lock();
+    std::fill(voltage_avg.begin(), voltage_avg.end(), 0);
 
-    std::cout << "Voltage read:\n\tCH1\tCH2\tCH3\tCH4\n"
-              << std::setprecision(2);
-    for (auto v : voltage_read) {
-        std::cout << "\t" << v;
+    for (int i = 0; i < sample_rate; i++){
+        std::vector<float> voltage_read = ADC.readVoltage(1, read_num);
+        if (voltage_read.empty()) {
+            return;
+        }
+        std::transform(voltage_avg.begin(), voltage_avg.end(),
+                       voltage_read.begin(),
+                       voltage_avg.begin(), std::plus<float>());
     }
-    std::cout << "\n";
+    // Calculate averages
+    for (auto &v : voltage_avg) {
+        v /= sample_rate;
+    }
+    voltage_avg_mutex.unlock();
 }
 
 void publishSensorData(const char* topic, std::string name, float value) {
@@ -122,7 +125,7 @@ void publishSensorData(const char* topic, std::string name, float value) {
     msg += "}";
 
     matrix752.publish(NULL, topic, msg.size(), msg.c_str(), 1);
-    std::cout << msg << "\n";        
+    //std::cout << msg << "\n";
 }
 
 int main() {
@@ -131,13 +134,15 @@ int main() {
         std::cout << "." << std::flush;
         std::this_thread::sleep_for(1s);
     }
-    std::cout << std::endl;
+    std::cout << "done" << std::endl;
 
     std::cout << "Connecting to server..." << std::flush;
     while (matrix752.connect_async(SERVER, TCP_PORT) != MOSQ_ERR_SUCCESS) {
         std::cout << "." << std::flush;
         std::this_thread::sleep_for(1s);
     }
+    std::cout << "done" << std::endl;
+
     matrix752.loop_start();
 
     std::thread temp_reader(readTemp);
@@ -150,20 +155,22 @@ int main() {
     fph_reader.detach();
 
     while (true) {
-        for (int i = 0; i < sample_rate; i++) {
-            readVoltage();
-            std::this_thread::sleep_for(milliseconds(1000/sample_rate));
+        readVoltage();
+        voltage_avg_mutex.lock();
+        std::cout << "Average voltage read:\n"
+                  << "\tCH1\tCH2\tCH3\tCH4\n"
+                  << std::setprecision(2);
+        for (const auto &v : voltage_avg) {
+            std::cout << "\t" << v;
         }
-
-        // Calculate averages
-        for (auto &v : voltage_avg) {
-            v /= sample_rate;
-        }
-
+        std::cout << std::endl;
+        voltage_avg_mutex.unlock();
         // Publish sensor data
         publishSensorData(tmp_topic, "Temperature", tmp);
         publishSensorData(odo_topic, "Dissolved oxygen", odo);
         publishSensorData(fph_topic, "pH", fph);
+
+        std::this_thread::sleep_for(1s);
     }
 
     matrix752.loop_stop();
